6d0ca65458d8373c005e934af4b7fc5367c35294
maniic <abdullahchabaytah@gmail.com>
2026-01-04 01:43:17 +0300

    Add __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Predictive Maintenance Digital Twin
+
+A machine learning system for predicting Remaining Useful Life (RUL) 
+of turbofan jet engines using NASA C-MAPSS dataset.
+"""
+
+__version__ = "0.1.0"
+
